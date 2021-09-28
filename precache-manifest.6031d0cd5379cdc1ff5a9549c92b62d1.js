677559ecbaef9e31923692875a68f8643b56802e
David McLaughlin <davidmclaughlin208@gmail.com>
2021-09-28 11:43:05 -0700

    Updates

---
+++ precache-manifest.6031d0cd5379cdc1ff5a9549c92b62d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585a0eff09c676de8ac6d0b08475e1dc",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "6ce68a13092149b2234a",
+    "url": "/home/static/css/main.4e794dd0.chunk.css"
+  },
+  {
+    "revision": "fd72f6a45f42f5218d90",
+    "url": "/home/static/js/2.ec6b3fbe.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.ec6b3fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce68a13092149b2234a",
+    "url": "/home/static/js/main.3b52f6cf.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "abf097fe3407fb2fbbd1db96838069be",
+    "url": "/home/static/media/DavidMcLaughlin.abf097fe.JPG"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4153599ab01d7f1bd9d3e50436c77ace.js"
+  "/home/precache-manifest.6031d0cd5379cdc1ff5a9549c92b62d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
